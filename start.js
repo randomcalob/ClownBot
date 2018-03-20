994a2976112540ca9a45053f62a803eaf6257c63
randomcalob <calob@uw.edu>
2018-03-20 10:34:30 -0700

    come and leave
    They fucking work!!!

--- start.js
+++ start.js
@@ -5,35 +5,49 @@ const token = settings.token
 
 const bot = new Discord.Client({disableEveryone: true})
 
-console.log(token)
-
-
-
 bot.on("ready", async () => {
 	console.log('bot is ready')
-	bot.generateInvite(["ADMINISTRATOR"]).then(link =>
-		console.log(link))
+	// bot.generateInvite(["ADMINISTRATOR"]).then(link =>
+	// 	console.log(link))
 	
 })
 bot.on('message', async message => {
 	if (!message.guild) return
-
 	let msg = message.content.split(" ")
 	let command = msg[0]
-	if (!command.startsWith(prefix)) return
-		command = command.substr(3)
-		console.log(command)
-	if (command === "hehe")
+	if (!command.startsWith(prefix)) {
+		return
+	}
+	command = command.substr(3)
+
+
+
+	if (command === "hehe") {
 		message.channel.send('heehee')
+	}
+
+
+
 	if (command === "come"){
-		message.member.voiceChannel.join().then(connection => {
-			message.reply("Infiltration Complete.")
-		})
-		.catch(console.log)
+		if (message.member.voiceChannel.joinable){
+			
+		// 	console.log("let's goooooo")
+		// 	message.member.voiceChannel.join().then(connection => {
+		// 		message.reply("Infiltration Complete.")
+
+		// })
+		// .catch(console.error)
+		}
+		else{
+			message.reply("Get in a fucking channel")
+		}
 	}
-	if (command === "leave"){
-		bot.voiceChannel.leave()
-	}
+
+
+
+	// if (command === "leave"){
+	// 	message.guild.me.voiceChannel.leave()
+	// }
 })
 
 bot.login(token)
